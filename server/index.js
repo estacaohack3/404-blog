a83ade4ca1ca848c8a3c146008b8ce799d367167
Bruno Pimentel <brunoapimentel@gmail.com>
2018-03-16 14:02:59 -0300

    Adiciona integração com BD

--- server/index.js
+++ server/index.js
@@ -9,7 +9,30 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Vitas!! trltrltrltrtlrtrl');
+  req.db.collection('posts').find().toArray((err, dados) => {
+    res.send(dados);
+  });
+});
+
+app.post('/', (req, res) => {
+  let dados = {
+    titulo: req.body.titulo,
+    autor: req.body.autor,
+    data: req.body.data,
+    conteudo: req.body.conteudo,
+    imagem: req.body.imagem
+  };
+
+  //validação
+  if(!dados.titulo || !dados.autor || !dados.conteudo){
+    res.status(400).send('É necessário enviar titulo, autor e conteúdo');
+    return;
+  }
+
+  req.db.collection('posts').insert(dados, (err, dados) => {
+    res.send(dados);
+  });  
+
 });
 
 app.listen(3030, () => {
